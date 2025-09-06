d34eadbb07f45f19e0382d2a6fb25852d240e0c5
Andrew <reynold@bc.edu>
2025-09-06 12:18:25 -0400

    Parts 1-4 of ProblemSet1

--- ps1-part2.py
+++ ps1-part2.py
@@ -1,11 +1,10 @@
-# Write a function that takes two variables and does all the computations asked
+def number_fun(a, b):
+    print(f"You entered {a} and {b}")
+    print(f"{a} + {b} = {a + b}")
+    print(f"{a} * {b} = {a * b}")
+    print(f"{a} ** {b} = {a ** b}")
+    print(f"{a} % {b} = {a % b}")
 
-# Ask the user for the first number, store the value in a variable
-
-
-# Ask the user for the second number, store the value in a variable
-
-
-# Repeat back the numbers
-
-# Perform calculations. Be careful about string formatting for autograders.
+firstnum = (int)(input("Enter an integer between 10 and 100: "))
+secondnum = (int)(input("Enter an integer less than 4: "))
+number_fun(firstnum, secondnum)
--- ps1-part4.py
+++ ps1-part4.py
@@ -1,1 +1,17 @@
+priorArrests = (int)(input('Prior arrests: '))
+localOrdinanceArrests = (input('Prior arrests for local ordinance (Y/N): '))
+releaseAge = (int)(input('Age at release: '))
 
+recidivismRisk = 0
+if priorArrests >= 2:
+    recidivismRisk += 1
+if priorArrests >= 5:
+    recidivismRisk += 1
+if localOrdinanceArrests == 'Y':
+    recidivismRisk += 1
+if releaseAge >= 18 and releaseAge <= 24:
+    recidivismRisk += 1
+if releaseAge >= 40:
+    recidivismRisk -= 1
+
+print(f'The recidivism risk score is {recidivismRisk}')
